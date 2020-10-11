c3497557b0748746ccb9c517a9f0c762cc4883d9
monda00 <monda0524@gmail.com>
2020-10-11 10:20:49 +0900

    add text

--- App.js
+++ App.js
@@ -7,9 +7,37 @@ import BodyText from './src/elements/BodyText';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!!!</Text>
-      <BodyText />
-      <StatusBar style="auto" />
+      <View style={styles.appbar}>
+        <View>
+          <Text>MEMOT</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講義のアイディア</Text>
+          <Text style={styles.memoDate}>2020/10/10</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講義のアイディア</Text>
+          <Text style={styles.memoDate}>2020/10/10</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講義のアイディア</Text>
+          <Text style={styles.memoDate}>2020/10/10</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講義のアイディア</Text>
+          <Text style={styles.memoDate}>2020/10/10</Text>
+        </View>
+
+        <View>
+          <Text>+</Text>
+        </View>
+      </View>
     </View>
   );
 }
